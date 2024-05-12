d99ffee5a395fe0c0af3468a9f8c33fbbeb1e67d
박유정 <p_1321@duksung.ac.kr>
2024-05-12 22:38:09 +0900

    feat: 홈 화면 ui 구현

---
+++ src/components/AlbumJacket.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { View, Image, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+interface AlbumJacketProps {
+  imageSource?: any;
+  text: string;
+}
+
+const AlbumJacket: React.FC<AlbumJacketProps> = ({ imageSource, text }) => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <View style={styles.imageContainer}>
+        {imageSource && typeof imageSource === 'string' ? (
+          <Image source={{ uri: imageSource }} style={styles.image} />
+        ) : (
+          <Image source={imageSource || require('../assets/image/Slumbus_Logo.png')} style={styles.image} />
+        )}
+        <View style={styles.playIcon}>
+          <Icon name="play" size={32} color="#fff" />
+        </View>
+      </View>
+      <Text style={styles.text}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginHorizontal: 12,
+  },
+  imageContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: '#dea',
+    marginBottom: 1,
+  },
+  image: {
+    width: 90,
+    height: 90,
+    borderRadius: 5,
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  playIcon: {
+    position: 'absolute',
+    zIndex: 1,
+  },
+});
+
+export default AlbumJacket;
