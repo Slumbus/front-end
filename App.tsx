0b6aa6700cb82f666f4826cdec272b345350ea34
박유정 <p_1321@duksung.ac.kr>
2024-06-02 15:18:51 +0900

    fix: 제스처 핸들러 설정

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import MainNavigator from './src/navigation/MainNavigator';
 
 
@@ -9,10 +11,12 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="MainNavigator" component={MainNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{flex:1}}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="MainNavigator" component={MainNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   )
 }
