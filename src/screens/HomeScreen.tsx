9b6c69beaf84ad9741a0a2b46c2d19132e80d004
박유정 <p_1321@duksung.ac.kr>
2024-07-13 17:14:11 +0900

    feat: 홈 화면 api 연결

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
+import axios from 'axios';
 
 import AlbumTitleText from '../components/AlbumTitleText';
 import AlbumJacket from '../components/AlbumJacket';
@@ -12,9 +13,27 @@ const events = [
   Event.PlaybackActiveTrackChanged,
 ];
 
+type MusicItem = { // 엔티티 수정 필요
+  userId: number;
+  kidId: number;
+  music: string;
+  title: string;
+  picture: string;
+  lyric: string | null;
+};
+
+type KidAlbum = {
+  kidId: number;
+  kidName: string;
+  kidPicture: string;
+  musicList: MusicItem[];
+};
+
 export default function HomeScreen({navigation}: any) {
   const [currentAlbum, setCurrentAlbum] = useState<any | undefined>();
   const [curremtTrack, setCurrentTrack] = useState<any | undefined>();
+  const [childrenAlbumData, setChildrenAlbumData] = useState<KidAlbum[]>([]);
+  const token = ``; // 로그인 기능 구현 후 수정 필요
 
   const { setIsPlaying } = usePlayback();
 
@@ -116,14 +135,32 @@ export default function HomeScreen({navigation}: any) {
     },
   ];
 
+  const fetchAlbumData = async () => {
+    try {
+      const response = await axios.get('http://10.0.2.2:8080/api/song/home', { // 로컬 서버 연결
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      });
+      console.log(response.data.data);
+      setChildrenAlbumData(response.data.data);
+    } catch (error) {
+      console.error('Error fetching album data', error);
+    }
+  };
+  
+  useEffect(() => {
+    fetchAlbumData();
+  }, []);
+
   const setSongList = async (index: number, songId: number) => {
-    setCurrentAlbum(ChildrenAlbumdata[index]);
+    setCurrentAlbum(childrenAlbumData[index]);
     const addTrack = async () => {
       try {
         await TrackPlayer.reset();
         console.log('TrackPlayer 초기화 성공');
         
-        await TrackPlayer.add(ChildrenAlbumdata[index].Music);
+        await TrackPlayer.add(childrenAlbumData[index].musicList);
         await TrackPlayer.setRepeatMode(RepeatMode.Off); // Off: 큐 반복 재생x, track: 한곡만 재생, Queue 전체 목록 재생
         const mode = await TrackPlayer.getRepeatMode();
         console.log(mode);
@@ -141,8 +178,8 @@ export default function HomeScreen({navigation}: any) {
       await TrackPlayer.play();
       setIsPlaying(true);
       navigation.navigate('PlayScreen', { // 더미데이터 값 직접 전달, api 연결 시 수정
-        album: ChildrenAlbumdata,
-        song: ChildrenAlbumdata[index].Music[songId],
+        album: childrenAlbumData,
+        song: childrenAlbumData[index].musicList[songId],
         // trackData:  //추후 여기에 앨범 트랙 데이터 넘겨주어야 함.
       });
       console.log('TrackPlayer 시작 성공');
@@ -158,16 +195,16 @@ export default function HomeScreen({navigation}: any) {
     <View style={styles.container}>
       <ScrollView>
         <View style={styles.albums}>
-          {ChildrenAlbumdata.map((album) => (
-            <View key={album.albumname}>
-              <AlbumTitleText imageSource={{ uri: album.picture}} text1= {album.name} text2={album.albumname} />
+          {childrenAlbumData.map((album) => (
+            <View key={album.kidId}>
+              <AlbumTitleText imageSource={{ uri: album.kidPicture}} text= {album.kidName} />
               <View style={styles.jackets}>
-                {album.Music.map((song) => (
+                {album.musicList.map((song) => (
                   <AlbumJacket 
-                  key={song.id} 
-                  imageSource={{ uri: song.artwork}} 
-                  text={song.title} 
-                  onPress={() => {setSongList(album.id, song.id);}} />
+                    key={song.id} // 수정 필요
+                    imageSource={{ uri: song.picture}} 
+                    text={song.title} 
+                    onPress={() => {setSongList(album.kidId, song.id);}} />
                 ))}
               </View>
             </View>
